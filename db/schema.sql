08b5ec6f5dcaa7b25ce193675332d44f557e2052
lavanya128 <lavanya128@gmail.com>
2017-11-03 19:55:28 -0400

    partially working

---
+++ db/schema.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE burgers_db;
+
+USE burgers_db;
+
+CREATE TABLE burgers(
+id INT NOT NULL AUTO_INCREMENT,
+burger_name VARCHAR(255),
+devoured BOOLEAN DEFAULT false,
+ts TIMESTAMP DEFAULT CURRENT_TIMESTAMP ,
+PRIMARY KEY(id));
---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers (burger_name, devoured) VALUES (Chicken Zinger Buger, false);
+INSERT INTO burgers (burger_name) VALUES (Double Mac Burger);
+INSERT INTO burgers (burger_name) VALUES (Grilled Chicken Burger);
+INSERT INTO burgers (burger_name, devoured) VALUES ();
+INSERT INTO burgers (burger_name, devoured) VALUES ();
+INSERT INTO burgers (burger_name, devoured) VALUES ();
