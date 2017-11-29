b1b7e811621e4857c60e27230b46ca6177459ec7
lavanya128 <lavanya128@gmail.com>
2017-11-28 20:16:08 -0500

    deployed in heroku wth jawsdb

--- config/connection.js
+++ config/connection.js
@@ -1,12 +1,30 @@
 var mysql = require("mysql");
 
-var connection = mysql.createConnection({
+var connection;
+
+// = mysql.createConnection({
+//   port: 8889,
+//   host: "localhost",
+//   user: "root",
+//   password: "root",
+//   database: "burgers_db"
+// });
+
+if(process.env.JAWSDB_URL)
+{
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+
+}
+else
+{
+  connection = mysql.createConnection({
   port: 8889,
   host: "localhost",
   user: "root",
   password: "root",
   database: "burgers_db"
 });
+}
 
 // Make connection.
 connection.connect(function(err) {
